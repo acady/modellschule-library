daf5ca6cb3c9d4ead0304d8310107bb60775b206
Stefan Zedlacher <stefan.zedlacher@gmx.at>
2023-05-16 13:39:48 +0200

    Counter

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+let counter = 0
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . . . .
+    . . # . .
+    `)
+for (let index = 0; index < 4; index++) {
+    counter += 1
+    basic.showNumber(counter)
+}
 basic.forever(function () {
 	
 })
